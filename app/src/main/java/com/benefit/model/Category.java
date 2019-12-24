7125c196a816d2d4e1e5092f9627c2d875c1cb3b
aviadud <aviadud@gmail.com>
2019-12-24 13:40:31 +0200

    add category

---
+++ app/src/main/java/com/benefit/model/Category.java
@@ -0,0 +1,10 @@
+package com.benefit.model;
+
+/**
+ *  Model POJO for category.
+ */
+public class Category {
+    private String name;
+    private String parentId;
+    private boolean isLeaf;
+}
