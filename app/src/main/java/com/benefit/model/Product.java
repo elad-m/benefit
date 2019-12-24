ccaa3f0f0a75ef8cfbfaa61af44483d8817e6a2a
aviadud <aviadud@gmail.com>
2019-12-24 13:47:52 +0200

    add property name and edited product

--- app/src/main/java/com/benefit/model/Product.java
+++ app/src/main/java/com/benefit/model/Product.java
@@ -1,15 +1,20 @@
 package com.benefit.model;
 
+import android.util.Pair;
+
 import com.google.firebase.firestore.ServerTimestamp;
 
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Model POJO for product.
  */
 public class Product {
     private String categoryId;
+    private Map<String, String> propertiesAndValues;
     private String SellerId;
     private String titleText;
     private String DescriptionText;
---
+++ app/src/main/java/com/benefit/model/PropertyName.java
@@ -0,0 +1,11 @@
+package com.benefit.model;
+
+/**
+ * Model POJO for property name.
+ */
+public class PropertyName {
+    private String categoryId;
+    private String name;
+    private String sortOrder;
+    private boolean isMandatory;
+}
