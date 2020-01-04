fd735593578e1733913134533649e00d703db539
izipris <idan.zipris@mail.huji.ac.il>
2020-01-04 16:54:31 +0200

    updated ProductService based on the new method in DatabaseDriver

--- app/src/main/java/com/benefit/services/ProductService.java
+++ app/src/main/java/com/benefit/services/ProductService.java
@@ -1,22 +1,15 @@
 package com.benefit.services;
 
-import androidx.annotation.NonNull;
-
 import com.benefit.drivers.DatabaseDriver;
 import com.benefit.model.Product;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
 
 public class ProductService {
     private DatabaseDriver databaseDriver;
     private CollectionReference productsCollection;
+    private static final String COLLECTION_NAME = "products";
 
     public ProductService(DatabaseDriver databaseDriver) {
         this.databaseDriver = databaseDriver;
@@ -28,38 +21,12 @@ public class ProductService {
     }
 
     public Product getProductsById(int productId) {
-        final List<Product> productList = new LinkedList<>();
-        productsCollection
-                .whereEqualTo("id", productId)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot productDocument : Objects.requireNonNull(task.getResult())) {
-                                productList.add(productDocument.toObject(Product.class));
-                            }
-                        }
-                    }
-                });
+        List<Product> productList = this.databaseDriver.getDocumentsByField(
+                COLLECTION_NAME, "id", productId, Product.class);
         return productList.get(0);
     }
 
     public List<Product> getProductsByCategoryId(int categoryId) {
-        final List<Product> productList = new LinkedList<>();
-        productsCollection
-                .whereEqualTo("categoryId", categoryId)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot productDocument : Objects.requireNonNull(task.getResult())) {
-                                productList.add(productDocument.toObject(Product.class));
-                            }
-                        }
-                    }
-                });
-        return productList;
+        return this.databaseDriver.getDocumentsByField(COLLECTION_NAME, "categoryId", categoryId, Product.class);
     }
 }
