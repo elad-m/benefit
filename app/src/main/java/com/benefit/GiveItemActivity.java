f4b380b56ff31d4d7df8eb0ff6f9bacfa8f85fdb
elad-m <musbae7@gmail.com>
2020-01-05 22:49:51 +0200

    1. Changed Java class activities names to contain the word 'Activity'. Also changed 'OfferItemActivity' to 'GiveItemActivity' 2. Added the 'thank you' popup dialog after pressing 'Give'. Also created a product. 3. Added missing implementation in gradle.build

--- app/src/main/java/com/benefit/GiveItemActivity.java
+++ app/src/main/java/com/benefit/GiveItemActivity.java
@@ -1,5 +1,6 @@
 package com.benefit;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,37 +15,51 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.benefit.model.Product;
 import com.squareup.picasso.Picasso;
 
-public class OfferItem extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This activity is used when the user wants to give/upload/offer to his profile.
+ */
+public class GiveItemActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 1;
+    private static final int SNAP_PHOTO = 2;
 
     private ImageButton mImageButtonUpload;
     private Uri mImageUri;
 
-    RadioGroup sizeRadioGroup;
-    RadioButton sizeRadioButton;
-    RadioGroup categoryRadioGroup;
-    RadioButton categoryRadioButton;
-    RadioGroup clothinDemographicRadioGroup;
-    RadioButton clothinDemographicRadioButton;
+    private RadioGroup sizeRadioGroup;
+    private RadioButton sizeRadioButton;
+    private RadioGroup categoryRadioGroup;
+    private RadioButton categoryRadioButton;
+    private RadioGroup clothingDemographicRadioGroup;
+    private RadioButton clothingDemographicRadioButton;
 
-    EditText mEdTextTitle;
-    EditText mEdTextDescription;
+    private EditText mEdTextTitle;
+    private EditText mEdTextDescription;
 
     private String mItemTitle;
     private String mItemDescription;
+
+    private Dialog mThankYouMessage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_offer_item);
+        setContentView(R.layout.activity_give_item);
 
         sizeRadioGroup = findViewById(R.id.item_size_radio_group);
         categoryRadioGroup = findViewById(R.id.item_category_radio_group);
-        clothinDemographicRadioGroup = findViewById(R.id.item_clothing_demographic_radio_group);
+        clothingDemographicRadioGroup = findViewById(R.id.item_clothing_demographic_radio_group);
 
         mEdTextTitle = findViewById(R.id.item_title_text);
         mEdTextDescription = findViewById(R.id.item_description_text);
@@ -102,20 +117,46 @@ public class OfferItem extends AppCompatActivity {
     }
 
 
-    public void setClothinDemographicRadioButton(View v) {
-        int radioId = clothinDemographicRadioGroup.getCheckedRadioButtonId();
-        clothinDemographicRadioButton = findViewById(radioId);
+    public void setClothingDemographicRadioButton(View v) {
+        int radioId = clothingDemographicRadioGroup.getCheckedRadioButtonId();
+        clothingDemographicRadioButton = findViewById(radioId);
 
-        Toast.makeText(this, "selected for: " + clothinDemographicRadioButton.getText(),
+        Toast.makeText(this, "selected for: " + clothingDemographicRadioButton.getText(),
                 Toast.LENGTH_SHORT).show();
     }
 
+    private Map<String, List<String>> getProductProperties() {
+        Map<String, List<String>> properties = new HashMap<>();
+        // todo: add actual list? WHY LIST?
+        properties.put("Size", (new LinkedList<String>()));
+        properties.put("Category", new LinkedList<String>());
+        properties.put("Demographic", new LinkedList<String>());
+        return properties;
+    }
 
-    public void createItem(View view) {
 
-        // the next lines are just to see the text is saved
+    public void onClickGive(View view) {
         mItemTitle = mEdTextTitle.getText().toString();
         mItemDescription = mEdTextDescription.getText().toString();
+        Date date = Calendar.getInstance().getTime();
+        Map<String, List<String>> properties = getProductProperties();
+        List<String> imagesUrls = new LinkedList<>();
+        imagesUrls.add(mImageUri.toString());  // todo: hope this cuts it
+        // todo: change to real id
+        Product product = new Product(64, 3, "sellerID", mItemTitle, mItemDescription,
+                0, 0, date, properties, imagesUrls);
+        // Now serializing should start, of product (and user)
+
+        // Thank you message
+        mThankYouMessage = new Dialog(this);
+        mThankYouMessage.setContentView(R.layout.thank_you_message);
+        mThankYouMessage.show();
+    }
+
+    public void done(View view) {
+        mThankYouMessage.dismiss();
+        Intent intent = new Intent(this, UserProfileActivity.class);
+        startActivity(intent);
     }
 
 
