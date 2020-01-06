06b98aa490fadf3f4c427e81b77a09e9796decd2
aviadud <aviadud@gmail.com>
2020-01-06 15:03:32 +0200

    add more to chat

--- app/src/main/java/com/benefit/ChatActivityMain.java
+++ app/src/main/java/com/benefit/ChatActivityMain.java
@@ -1,14 +1,29 @@
 package com.benefit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
+import com.benefit.drivers.DatabaseDriver;
+import com.benefit.viewmodel.ChatActivityViewModel;
+import com.benefit.viewmodel.SignInViewModel;
+
 public class ChatActivityMain extends AppCompatActivity {
+
+    private DatabaseDriver databaseDriver;
+    private ChatActivityViewModel cViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_main);
+
+        databaseDriver = new DatabaseDriver();
+
+        // View model
+        cViewModel = ViewModelProviders.of(this).get(ChatActivityViewModel.class);
+
     }
 }
