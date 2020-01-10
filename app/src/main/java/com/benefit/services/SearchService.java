803c39ad497ca52c5db4579d03445869c55a5158
izipris <idan.zipris@mail.huji.ac.il>
2020-01-10 16:20:28 +0200

    implemented search methods in SearchService

--- app/src/main/java/com/benefit/services/SearchService.java
+++ app/src/main/java/com/benefit/services/SearchService.java
@@ -1,9 +1,25 @@
 package com.benefit.services;
 
-import com.benefit.drivers.DatabaseDriver;
-import com.google.firebase.firestore.CollectionReference;
+import android.util.Log;
 
-public class SearchService {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.benefit.drivers.DatabaseDriver;
+import com.benefit.model.Product;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * A service which responsible for search procedures
+ */
+public class SearchService extends ViewModel {
     private DatabaseDriver databaseDriver;
     private CollectionReference productsCollection;
     private static final String COLLECTION_NAME_PRODUCTS = "products";
@@ -12,5 +28,45 @@ public class SearchService {
     public SearchService(DatabaseDriver databaseDriver) {
         this.databaseDriver = databaseDriver;
         this.productsCollection = this.databaseDriver.getCollectionByName(COLLECTION_NAME_PRODUCTS);
+    }
+
+    public MutableLiveData<List<Product>> getProductsBySearchString(String searchQuery) {
+        return getProductsBySearchString(searchQuery, -1);
+    }
+
+    public MutableLiveData<List<Product>> getProductsBySearchString(String searchQuery, int categoryId) {
+        String[] keywords = searchQuery.split(" ");
+        final List<Product> productsList = new LinkedList<>();
+        final MutableLiveData<List<Product>> resultsLiveData = new MutableLiveData<>();
+        Task<QuerySnapshot> querySnapshotTask;
+        if (categoryId < 0) {
+            querySnapshotTask = this.databaseDriver.getCollectionByName(COLLECTION_NAME_PRODUCTS).get();
+        } else {
+            querySnapshotTask = this.databaseDriver.getCollectionByName(COLLECTION_NAME_PRODUCTS).whereEqualTo("categoryId", categoryId).get();
+
+        }
+        querySnapshotTask
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                            Product product = document.toObject(Product.class);
+                            if (isProductFitsKeywords(product, keywords)) {
+                                productsList.add(product);
+                            }
+                        }
+                        resultsLiveData.setValue(productsList);
+                    }
+                })
+                .addOnFailureListener(e -> Log.w(TAG, "Error on getProductsBySearchString", e));
+        return resultsLiveData;
+    }
+
+    private Boolean isProductFitsKeywords(Product product, String[] keywords) {
+        for (String keyword : keywords) {
+            if (!(product.getTitle().contains(keyword) && product.getDescription().contains(keyword))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
