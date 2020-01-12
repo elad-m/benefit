1bba0d1457de02f67dea40a72311870162bf6273
aviad dudkevich <aviad.dudkevich@mail.huji.ac.il>
2020-01-12 17:19:45 +0200

    finish conversationAdapter

--- app/src/main/java/com/benefit/adapters/ConversationAdapter.java
+++ app/src/main/java/com/benefit/adapters/ConversationAdapter.java
@@ -1,18 +1,32 @@
 package com.benefit.adapters;
 
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.benefit.R;
+import com.benefit.drivers.DatabaseDriver;
 import com.benefit.model.Chat;
 import com.benefit.model.Match;
+import com.benefit.model.Product;
+import com.benefit.model.User;
+import com.benefit.services.ProductService;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+import io.opencensus.resource.Resource;
 
 /**
  * An adapter for conversation queries
@@ -20,17 +34,37 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 public class ConversationAdapter extends FirestoreRecyclerAdapter<Match, ConversationAdapter.ConversationHolder> {
 
     static class ConversationHolder extends RecyclerView.ViewHolder {
-        TextView productTitle, productOwner, lastChatDate;
+        TextView productTitle, productOwnerName, lastChatDate, giveOrWantText;
+        Product product;
+        final DateFormat dateFormat = new SimpleDateFormat("h:mm a yy.MM.dd");
         ConversationHolder(View v) {
             super(v);
             productTitle = itemView.findViewById(R.id.product_title);
-            productOwner = itemView.findViewById(R.id.owner_name);
+            productOwnerName = itemView.findViewById(R.id.owner_name);
             lastChatDate = itemView.findViewById(R.id.last_chat_time);
+            giveOrWantText = itemView.findViewById(R.id.give_or_want);
+        }
+
+        void setProduct(Product product) {
+            this.product = product;
+            this.productTitle.setText(product.getTitle());
+            //set picture
+        }
+
+        void setProductOwnerName(String productOwner) {
+            this.productOwnerName.setText(productOwner);
+        }
+
+        void setLastChatDate(Date date){
+            lastChatDate.setText(dateFormat.format(date));
         }
     }
 
-    public ConversationAdapter(@NonNull FirestoreRecyclerOptions<Match> options) {
+    private DatabaseDriver databaseDriver;
+
+    public ConversationAdapter(@NonNull FirestoreRecyclerOptions<Match> options, DatabaseDriver databaseDriver) {
         super(options);
+        this.databaseDriver = databaseDriver;
     }
 
     @NonNull
@@ -41,8 +75,34 @@ public class ConversationAdapter extends FirestoreRecyclerAdapter<Match, Convers
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull ConversationHolder chatMassageHolder, int i, @NonNull Match match) {
-
+    protected void onBindViewHolder(@NonNull ConversationHolder conversationHolder, int i, @NonNull Match match) {
+        conversationHolder.setLastChatDate(match.getTimestamp());
+        databaseDriver.getCollectionReferenceByName("products")
+                .whereEqualTo("id", match.getProductId())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                            conversationHolder.setProduct(document.toObject(Product.class));
+                        }
+                    }
+                });
+        if (match.getSellerId().equals(databaseDriver.getAuth().getUid())){
+            conversationHolder.setProductOwnerName(Resources.getSystem().getString(R.string.user_item));
+        }
+        else {
+            databaseDriver.getCollectionReferenceByName("users")
+                    .whereEqualTo("uid", match.getSellerId())
+                    .get()
+                    .addOnCompleteListener(task -> {
+                       if (task.isSuccessful()){
+                           for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                               User sellerUser = document.toObject(User.class);
+                               conversationHolder.setProductOwnerName(sellerUser.getFirstName() + " " + sellerUser.getLastName());
+                           }
+                       }
+                    });
+        }
     }
 
 }
