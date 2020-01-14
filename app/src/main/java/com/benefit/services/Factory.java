34a4d81e4705dc06354edcace43b10b8d955dad8
Idan Zipris <idan.zipris@mail.huji.ac.il>
2020-01-14 12:29:01 +0200

    added a factory for services

---
+++ app/src/main/java/com/benefit/services/Factory.java
@@ -0,0 +1,21 @@
+package com.benefit.services;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.benefit.drivers.DatabaseDriver;
+
+public class Factory {
+    private final static DatabaseDriver databaseDriver = new DatabaseDriver();
+
+    public static ViewModelProvider.Factory getProductServiceFactory() {
+        return new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new ProductService(databaseDriver);
+            }
+        };
+    }
+}
