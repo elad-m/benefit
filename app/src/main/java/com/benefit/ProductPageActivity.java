ed606b1cd852d618e527fad2a5819c958be23e8a
Idan Zipris <idan.zipris@mail.huji.ac.il>
2020-01-14 16:48:20 +0200

    added map fragment to product page

--- app/src/main/java/com/benefit/ProductPageActivity.java
+++ app/src/main/java/com/benefit/ProductPageActivity.java
@@ -1,5 +1,7 @@
 package com.benefit;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,16 +12,29 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.benefit.model.Product;
+import com.benefit.model.User;
 import com.benefit.services.Factory;
 import com.benefit.services.ProductService;
+import com.benefit.services.UserService;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
-public class ProductPageActivity extends AppCompatActivity {
+public class ProductPageActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private int productId = 1;
     private ProductService productService;
+    private UserService userService;
+
+    private GoogleMap mMap;
+    private Location mapLocation;
 
 
     @Override
@@ -27,6 +42,7 @@ public class ProductPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_page);
         this.productService = ViewModelProviders.of(this, Factory.getProductServiceFactory()).get(ProductService.class);
+        this.userService = ViewModelProviders.of(this, Factory.getUserServiceFactory()).get(UserService.class);
         final Observer<Product> productObserver = this::displayProductOnPage;
         productService.getProductById(this.productId).observe(this, productObserver);
     }
@@ -40,6 +56,8 @@ public class ProductPageActivity extends AppCompatActivity {
         textViewTitle.setText(product.getTitle());
         textViewDescription.setText(product.getDescription());
         textViewProperties.setText(getPropertiesString(product.getProperties()));
+        final Observer<User> userObserver = this::displayMap;
+        userService.getUserById(product.getSellerId()).observe(this, userObserver);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -57,5 +75,25 @@ public class ProductPageActivity extends AppCompatActivity {
                     .append(" ");
         }
         return propertiesString.toString();
+    }
+
+    private void displayMap(User user) {
+        this.mapLocation = new Location(LocationManager.GPS_PROVIDER);
+        this.mapLocation.setLongitude(user.getLocationLongitude());
+        this.mapLocation.setLatitude(user.getLocationLatitude());
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.productPageMapFragment);
+        Objects.requireNonNull(mapFragment).getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.mMap = googleMap;
+        LatLng coordinates = new LatLng(mapLocation.getLatitude(), mapLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions()
+                .position(coordinates)
+                .title("Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15));
     }
 }
