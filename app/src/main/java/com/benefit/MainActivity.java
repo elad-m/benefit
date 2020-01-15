8130dd3e337fe990a63c03964eda014e453fe2dc
Idan Zipris <idan.zipris@mail.huji.ac.il>
2020-01-15 23:38:04 +0200

    temporary changes for testing the StorageDriver

--- app/src/main/java/com/benefit/MainActivity.java
+++ app/src/main/java/com/benefit/MainActivity.java
@@ -1,24 +1,94 @@
 package com.benefit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import com.benefit.drivers.DatabaseDriver;
-import com.benefit.model.Category;
-import com.benefit.services.CategoryService;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.benefit.drivers.StorageDriver;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnChoose, btnUpload;
+    private ImageView imageView;
+    private StorageDriver storageDriver;
+
+    private Uri filePath;
+
+    private final int PICK_IMAGE_REQUEST = 71;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //LiveData
+        ViewModelProvider.Factory storageDriverFactory = new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new StorageDriver();
+            }
+        };
+        storageDriver = ViewModelProviders.of(this, storageDriverFactory).get(StorageDriver.class);
+        final Observer<String> observer = new Observer<String>() {
 
+            @Override
+            public void onChanged(String str) {
+                String uri = str;
+            }
+        };
+        //Initialize Views
+        btnChoose = (Button) findViewById(R.id.btnChoose);
+        btnUpload = (Button) findViewById(R.id.btnUpload);
+        imageView = (ImageView) findViewById(R.id.imgView);
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+        final MainActivity mainActivity = this;
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                storageDriver.uploadImage(filePath).observe(mainActivity, observer);
+            }
+        });
 
+    }
 
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
