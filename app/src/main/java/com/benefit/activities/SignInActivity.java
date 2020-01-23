41c6c6c21aca4d89283df95a4bb7a5b537076cc7
aviadud <aviadud@gmail.com>
2020-01-23 23:52:29 +0200

    Merge branch 'feature_integretion_with_sign_in' of C:\MyAndroidApps\benefit with conflicts.

--- app/src/main/java/com/benefit/activities/SignInActivity.java
+++ app/src/main/java/com/benefit/activities/SignInActivity.java
@@ -5,7 +5,6 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.telephony.PhoneNumberFormattingTextWatcher;
-import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
