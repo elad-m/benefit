8f7c935e657906a4e3e4edece87192feaa1fc68e
aviadud <aviadud@gmail.com>
2020-02-09 00:43:27 +0200

    add menu with sign out

--- app/src/main/java/com/benefit/activities/MainActivity2.java
+++ app/src/main/java/com/benefit/activities/MainActivity2.java
@@ -1,17 +1,21 @@
 package com.benefit.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
 import com.benefit.R;
+import com.benefit.drivers.AuthenticationDriver;
 import com.benefit.model.Category;
 import com.benefit.model.Product;
 import com.benefit.model.User;
@@ -21,16 +25,19 @@ import com.benefit.ui.GiveFragment;
 import com.benefit.ui.HomeFragment;
 import com.benefit.ui.ProductFragment;
 import com.benefit.ui.ProfileFragment;
+import com.google.android.material.navigation.NavigationView;
 
 /**
  * This is the main activity.
  */
 public class MainActivity2 extends AppCompatActivity {
 
+    private AuthenticationDriver authenticationDriver = new AuthenticationDriver();
     private View chosenView;
     private FrameLayout settingButton;
-    private Button homeButton, mainButton, userProfileButton, giveButton;
+    private Button homeButton, userProfileButton, giveButton;
     private Fragment activityFragment;
+    private NavigationView menu;
 
     private User user;
 
@@ -38,9 +45,6 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        if (savedInstanceState == null) {
-
-        }
         user = (User) getIntent().getSerializableExtra(getString(R.string.user_relay));
 
         initiateViewElements();
@@ -51,10 +55,20 @@ public class MainActivity2 extends AppCompatActivity {
     private void initiateViewElements(){
         chosenView = findViewById(R.id.chosen_view);
         settingButton = findViewById(R.id.sandwich_icon);
-        mainButton = findViewById(R.id.home_icon);
+        homeButton = findViewById(R.id.home_icon);
         userProfileButton = findViewById(R.id.user_icon);
         giveButton = findViewById(R.id.give_icon);
         activityFragment = getSupportFragmentManager().findFragmentById(R.id.activity_fragment);
+        menu = findViewById(R.id.setting_navigation);
+        menu.setNavigationItemSelectedListener(item -> {
+            if (item.getTitle().equals(getString(R.string.sign_out))){
+                authenticationDriver.getAuth().signOut();
+                Intent intent = new Intent(this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            return true;
+        });
     }
 
     private void replaceFragment(Fragment fragment){
@@ -79,7 +93,7 @@ public class MainActivity2 extends AppCompatActivity {
         chosenView.setVisibility(View.VISIBLE);
         giveButton.setBackground(getDrawable(R.drawable.ic_give_icon));
         userProfileButton.setBackground(getDrawable(R.drawable.ic_user_icon));
-        mainButton.setBackground(getDrawable(R.drawable.ic_home_colored));
+        homeButton.setBackground(getDrawable(R.drawable.ic_home_colored));
 
         replaceFragment(HomeFragment.getInstance(metaCategoryChosen));
     }
@@ -111,7 +125,7 @@ public class MainActivity2 extends AppCompatActivity {
         chosenView.setVisibility(View.VISIBLE);
         giveButton.setBackground(getDrawable(R.drawable.ic_give_icon));
         userProfileButton.setBackground(getDrawable(R.drawable.ic_user_colored));
-        mainButton.setBackground(getDrawable(R.drawable.ic_home_icon));
+        homeButton.setBackground(getDrawable(R.drawable.ic_home_icon));
 
         replaceFragment(ProfileFragment.newInstance(user));
     }
@@ -124,7 +138,7 @@ public class MainActivity2 extends AppCompatActivity {
         chosenView.setVisibility(View.INVISIBLE);
         giveButton.setBackground(getDrawable(R.drawable.ic_give_colored));
         userProfileButton.setBackground(getDrawable(R.drawable.ic_user_icon));
-        mainButton.setBackground(getDrawable(R.drawable.ic_home_icon));
+        homeButton.setBackground(getDrawable(R.drawable.ic_home_icon));
 
         replaceFragment(GiveFragment.newInstance(user, product));
     }
